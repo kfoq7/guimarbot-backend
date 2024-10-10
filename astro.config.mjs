eeec8309deb2b74e8564795f77e5dd3301d95afd
kfoq7 <frbrizoi@gmail.com>
2024-10-10 11:16:58 -0500

    feat: updated header and login and dashbaord page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +20,7 @@ export default defineConfig({
         usePolling: true
       }
     }
-  }
+  },
+
+  adapter: vercel()
 })
