ebc2b1ed9f90c22d3fb66bbfda7d41e7a2ac04e3
Maza92 <maza05599@gmail.com>
2024-10-24 11:31:49 -0500

    feat(*): flow user update added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,12 +9,26 @@ export default {
         sans: ['Inter Variable', ...defaultTheme.fontFamily.sans]
       },
       colors: {
-        'steel-blue': '#3d5a80',
+        /**
+         * 'steel-blue': '#3d5a80',
         'light-blue-sky': '#98c1d9',
         'pale-aqua': '#e0fbfc',
         'sunset-orange': '#ee6c4d',
         'midnight-navy': '#293241',
         'primary-dark': '#273b59'
+         */
+        child: {
+          primary: '#FFB6C1',  // Colores suaves para niños
+          background: '#FFF0F5',
+        },
+        teen: {
+          primary: '#ADD8E6',  // Colores frescos para adolescentes
+          background: '#F0F8FF',
+        },
+        adult: {
+          primary: '#4682B4',  // Colores más formales para adultos
+          background: '#E6E6FA',
+        },
       }
     },
     fontFamily: {
